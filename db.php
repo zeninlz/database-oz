95924526dd8095db726d10dff8d3d3d4c515186c
martino simic <131748598+zeninlz@users.noreply.github.com>
2023-11-27 22:12:13 +0100

    Add files via upload

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 class Database {
     public $pdo;
     
-    public function __construct($db= "user", $user= "root", $pass="", $host= "localhost"){
+    public function __construct($db= "users", $user= "root", $pass="", $host= "localhost"){
     
         try{
             $this->pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
@@ -15,13 +15,24 @@ class Database {
 
         }
     }       
-    public function optellen($a, $b){
-        $sql = "INSERT INTO student (email, password) VALUES (:email, :password)";
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([':email' => $a, 'password' => $b]);
-
+    public function insertUser($name, $password){
+        $stmt = $this->pdo->prepare("insert into user (name, password) values (?, ?)");
+        $stmt-> execute([$name, $password]);
     }
     
+
+    public function slectUser(){
+        $stmt = $this->pdo->query("SELECT *FROM user" );
+        $result = $stmt->fetchAll();
+        return $result;
+
+    }
+    public function slectOneUser($id){
+        $stmt = $this->pdo->prepare("SELECT *FROM user Where id = ?");
+        $stmt->execute([$id]);
+        $result = $stmt->fetch();
+        return $result;
+    }
 }
 
 ?> 
--- home.php
+++ home.php
@@ -3,13 +3,58 @@ include('db.php');
 
 $connetie = new Database();
 
-try{
-    $connetie->optellen("mar@gmail.com","1234");
-    echo"gelukt";
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    try{
+        $connetie->insertUser($_POST['name'], $_POST['password']);
+        echo "toevoeging gelukt"; 
 
-} catch  (PDOException $e)
-{ 
-    echo "error inserting' .".$e->getMessage();
+    }catch (Exception $e ){
+        echo $e->getMessage();
+    }
 }
 
 ?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<table>
+    <tr>
+        
+        <th>name</th>
+        <th>password</th>
+    </tr>
+
+    <tr>
+        <?php 
+        $users = $connetie->slectOneUser(7);?>
+         <td><?php echo $users['id'];?></td>
+        <td><?php echo $users['name'];?></td>
+        <td><?php echo $users['password'];?></td>
+    </tr>
+
+
+    <tr>
+        <?php 
+        $users = $connetie->slectUser();
+        foreach ($users as $user) {?>
+        <td><?php echo $user['name'];?></td>
+        <td><?php echo $user['password'];?></td>
+    </tr> <?php } ?>
+</table>
+
+
+
+
+    <form method="POST">
+        <input type="text" name="name">
+        <input type="password" name="password">
+        <input type="submit">
+    </form>
+</body>
+</html>
